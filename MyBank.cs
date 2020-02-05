a4c4c7a08265aeaaa55a7902cf8906514d6172f8
Mongrelgin <Mongrelgin@users.noreply.github.com>
2020-02-05 14:47:45 +0300

    Initial commit

---
+++ MyBank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBankLib;
+
+namespace MyBank
+{
+    class MyBank
+    {
+        static void Main(string[] args)
+        {
+            var AccFirst = new BankAccount("Ivan", 50000);
+            Console.WriteLine($"Account {AccFirst.Number} was created for {AccFirst.Owner} with {AccFirst.Balance}");
+            AccFirst.MakeDeposit(2000, DateTime.Now, "Present");
+            AccFirst.MakeWithdrawal(17000, DateTime.Now, "Watch");
+            Console.WriteLine(AccFirst.GetAccountHistory());
+            Console.WriteLine(AccFirst.Balance);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MyBank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyBank")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2f508410-443a-4910-8e16-7581b9d6acd9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
